0d783b3c9dd06705cc8bb0f9244be3db70970f00
Chigil <andreichigilei@gmail.com>
2022-07-08 15:51:40 +0300

    Fixed git-pages

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import NavBar from './NavBar/NavBar';
-import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import Context from '../Context/context';
 import Modal from './Modal/Modal';
